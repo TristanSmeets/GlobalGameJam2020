2c10bc8377e487b8fe1502ee3a1f8b13bbecb773
rpijpker01 <rimme.pijpker@gmail.com>
2020-01-31 21:21:57 +0100

    scripts folder

---
+++ GameJam2020/Assets/Scripts/ScreenGlitching.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class ScreenGlitching : MonoBehaviour
+{
+    [SerializeField]
+    private Material glitchMaterial;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        if (glitchMaterial)
+            Graphics.Blit(source, glitchMaterial);
+        else
+            Graphics.Blit(source, destination);
+    }
+}

74a5037a69ff4bac30e03860f604cf5528609ad5
Tristan Smeets <tristansmeets89@hotmail.com>
2020-02-01 00:12:10 +0100

    Added player controls

---
+++ GameJam2020/Assets/Scripts/Player/Controller.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    [SerializeField] float movementSpeed = 10.0f;
+    CharacterController controller;
+    Transform cachedTransform;
+
+    private void OnEnable()
+    {
+        cachedTransform = gameObject.transform;
+        controller = GetComponent<CharacterController>();
+    }
+
+    public void FixedUpdate()
+    {
+        Vector3 moveDirection = new Vector3(Input.GetAxis("LeftStickHorizontal"),0,Input.GetAxis("LeftStickVertical"));
+        Vector3 lookDirection = new Vector3(Input.GetAxis("RightStickVertical"), 0, Input.GetAxis("RightStickHorizontal"));
+
+        if(lookDirection.sqrMagnitude > .8f)
+        {
+            Quaternion lookRotation = Quaternion.LookRotation(lookDirection); 
+            cachedTransform.rotation = lookRotation;
+        }
+        controller.SimpleMove(moveDirection * movementSpeed);
+    }
+}

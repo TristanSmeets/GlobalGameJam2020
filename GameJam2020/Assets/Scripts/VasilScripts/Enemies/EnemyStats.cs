76bea660fa293298db959c888a03b1c1fdb08d71
Fasko <wasilev.8v@gmail.com>
2020-02-01 01:42:38 +0100

    Enemy Scripts: +AIBehavior +AIZerglingBehavior +AIMoveToTarget +EnemyStats
    GameManager Scripts:
    +GameStats
    +EnemySpawner

---
+++ GameJam2020/Assets/Scripts/VasilScripts/Enemies/EnemyStats.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyStats : MonoBehaviour
+{
+    [Header("\t\t\t\tHealth Settings")]
+    private int _baseHealth;
+    private int _baseStunHealth;
+    [Header("\t\t\t\tAttack Settings")]
+    private int _baseDamage;
+    private float _attackRange;
+    private float _waitTimeAfterAttack;
+
+    [HideInInspector]
+    public int CurrentHealth;
+    [HideInInspector]
+    public int CurrentStunHealth;
+
+    private void Start()
+    {
+        CurrentHealth = _baseHealth;
+        CurrentStunHealth = _baseStunHealth;
+    }
+
+    public int MaxHealth { get => _baseHealth; }
+    public int MaxStunHealth { get => _baseStunHealth; }
+    public int Damage { get => _baseDamage; }
+    public float AttackRange { get => _attackRange; }
+    public float WaitTimeAfterAttack { get => _waitTimeAfterAttack; }
+}
---
+++ GameJam2020/Assets/Scripts/VasilScripts/GameManager/EnemySpawner.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [Header("\t\t\tSpawn Distance from Player")]
+    [SerializeField]
+    private int _radius;
+    [SerializeField]
+    private float _maxTimeBetweenEnemySpawns;
+
+    private GameObject[] _enemyPrefabs;
+    private Transform _playerTransform;
+    private GameStats _gameStats;
+
+    private float _enemySpawnTimer = 0;
+
+    void Start()
+    {
+        _enemyPrefabs = Resources.LoadAll<GameObject>("Enemies/");
+        _playerTransform = GameObject.Find("Player").GetComponent<Transform>();
+        _gameStats = GetComponent<GameStats>();
+    }
+
+    void Update()
+    {
+        SpawnEnemyAroundPlayer();
+    }
+
+    void SpawnEnemyAroundPlayer()
+    {
+        if(_gameStats.MaxEnemiesAtOnce <= _gameStats.EnemiesInLevel.Count || _gameStats.RemainingEnemiesInWave <= 0)
+            return;
+
+        _enemySpawnTimer -= Time.deltaTime;
+        if(_enemySpawnTimer > 0)
+            return;
+
+        GameObject go = Instantiate(_enemyPrefabs[0]);
+        int point = Random.Range(0, 100);
+        Vector3 spawnPosition = new Vector3(_playerTransform.position.x + _radius * Mathf.Cos(point), _playerTransform.position.y, _playerTransform.position.z + _radius * Mathf.Sin(point));
+        RaycastHit hit;
+        while(!Physics.Raycast(spawnPosition, Vector3.down, out hit, 5))
+        {
+            point = Random.Range(0, 100);
+            spawnPosition = new Vector3(_playerTransform.position.x + _radius * Mathf.Cos(point), _playerTransform.position.y, _playerTransform.position.z + _radius * Mathf.Sin(point));
+        }
+        go.transform.position = spawnPosition;
+        go.transform.LookAt(_playerTransform);
+
+        _enemySpawnTimer = Random.Range(0.05f, _maxTimeBetweenEnemySpawns);
+    }
+}

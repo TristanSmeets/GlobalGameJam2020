9f666591930f83dcdfcabc29fc42630000eb62eb
Tristan Smeets <tristansmeets89@hotmail.com>
2020-02-01 15:47:27 +0100

    Added health indicator.

---
+++ GameJam2020/Assets/Scripts/UI/HealthIndicator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Doozy.Engine.Progress;
+
+public class HealthIndicator : MonoBehaviour
+{
+    private Progressor progressor;
+    private float maxHealth = 100.0f;
+    private Player.Player player;
+    private const string playerTag = "Player";
+
+    private void OnEnable()
+    {
+        player = GameObject.FindGameObjectWithTag(playerTag).GetComponent<Player.Player>();
+        progressor = GetComponentInChildren<Progressor>();
+        progressor.AnimateValue = true;
+        progressor.SetMax(player.GetPlayerStats().MaxHealth);
+        AddListeners();
+    }
+
+    private void OnDisable()
+    {
+        RemoveListeners();
+    }
+
+    private void AddListeners()
+    {
+        player.DamagedPlayer += OnDamagedPlayer;
+    }
+    private void RemoveListeners()
+    {
+        player.DamagedPlayer -= OnDamagedPlayer;
+    }
+
+    private void OnDamagedPlayer(float currentHealth)
+    {
+        Debug.LogFormat($"Current health: {currentHealth}");
+        progressor.SetValue(currentHealth);
+    }
+}

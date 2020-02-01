e0c3c203aba9608752250e34c86a2d71c316a0b2
Tristan Smeets <tristansmeets89@hotmail.com>
2020-02-01 23:53:07 +0100

    Added round start bar. Also added PopUp creator from RRD project.

---
+++ GameJam2020/Assets/Scripts/UI/RoundStartIndicator.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoundStartIndicator : MonoBehaviour
+{
+    PopupCreator popupCreator;
+
+    private void OnEnable()
+    {
+        popupCreator = GetComponent<PopupCreator>();
+        AddListeners();
+    }
+
+    private void OnDisable()
+    {
+        RemoveListeners();
+    }
+    private void AddListeners()
+    {
+        GameStats.OnRoundEnd += OnRoundEnd;
+    }
+    private void RemoveListeners()
+    {
+        GameStats.OnRoundEnd -= OnRoundEnd;
+    }
+    private void OnRoundEnd()
+    {
+        popupCreator.CreatePopup("RoundStartPopup");
+    }
+}

28fce6b33687203dbf31e75bea7f6e7b99396517
Bulhard1 <33345325+Bulhard1@users.noreply.github.com>
2020-02-02 11:57:03 +0100

    dd

---
+++ GameJam2020/Assets/Scripts/UI/PauseSpawner.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseSpawner : MonoBehaviour
+{
+    PopupCreator popupCreator = null;
+    private const string pausePopup = "PausePopup";
+
+    private void OnEnable()
+    {
+        popupCreator = GetComponent<PopupCreator>();
+    }
+   
+    // Update is called once per frame
+    void Update()
+    {
+       if(Input.GetAxis("Pause") > 0)
+        {
+            popupCreator.CreatePopup(pausePopup);
+        }
+    }
+}

63c1ce51cd43798892487b67d926dafadf9059eb
Tristan Smeets <tristansmeets89@hotmail.com>
2020-02-02 13:53:58 +0100

    Added a sceneloading component.

---
+++ GameJam2020/Assets/Scripts/SceneLoading.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class SceneLoading : MonoBehaviour
+{
+   public void LoadScene(string sceneName)
+    {
+        StopAllCoroutines();
+        StartCoroutine(StartLoading(sceneName));
+    }
+
+    private IEnumerator StartLoading(string sceneName)
+    {
+        AsyncOperation asyncOperation = SceneManager.LoadSceneAsync(sceneName);
+
+        asyncOperation.allowSceneActivation = true;
+
+        if(Time.timeScale == 0)
+        {
+            Time.timeScale = 1;
+        }
+
+        while (!asyncOperation.isDone)
+        {
+        }
+            yield return null;
+    }
+}

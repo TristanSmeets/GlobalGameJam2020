e441896a11abc461b7712ab18194ba252e620bf4
rpijpker01 <rimme.pijpker@gmail.com>
2020-02-02 03:50:50 +0100

    Feedback, changed textures, more things

---
+++ GameJam2020/Assets/Scripts/Effects/PlayerGlitcher.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerGlitcher : MonoBehaviour
+{
+    private Renderer renderer;
+    private Player.Player player;
+
+    [SerializeField]
+    private bool glitch = false;
+    float glitchVal = 0;
+    [SerializeField]
+    private float glitchSpeed = 10;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        renderer = GetComponent<Renderer>();
+        player = GameObject.Find("Player").GetComponent<Player.Player>();
+        player.DamagedPlayer += StartGlitch;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(glitch)
+        {
+            glitchVal = Mathf.Lerp(glitchVal, 1, Time.deltaTime * glitchSpeed);
+
+            if(glitchVal >= 0.99)
+            {
+                glitch = false;
+                glitchVal = 0;
+            }
+        }
+        renderer.material.SetFloat("_Value", glitchVal);
+    }
+
+    public void StartGlitch(float currentPlayerHealth)
+    {
+        glitch = true;
+    }
+}

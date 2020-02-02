41d3dea5be0cb45c51a1edceeacdb6004ca38f39
Tristan Smeets <tristansmeets89@hotmail.com>
2020-02-02 13:12:20 +0100

    MainMenu.created loads

--- GameJam2020/Assets/Scripts/UI/MainMenuFunctionality.cs
+++ GameJam2020/Assets/Scripts/UI/MainMenuFunctionality.cs
@@ -1,9 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.SceneManagement;
 
 public class MainMenuFunctionality : MonoBehaviour
 {
+    [SerializeField] VideoPlayer videoPlayer = null;
+    [SerializeField] private string scene = string.Empty;
+    private bool startedGame = false;
+
     public void CloseApplication()
     {
         Application.Quit();
@@ -11,5 +17,33 @@ public class MainMenuFunctionality : MonoBehaviour
 
     public void StartGame()
     {
+        if(startedGame)
+        {
+            return;
+        }
+        videoPlayer.Play();
+        StartCoroutine(LoadSceneAfterVideo(scene));
+    }
+
+    private IEnumerator LoadSceneAfterVideo(string sceneName)
+    {
+        AsyncOperation async = SceneManager.LoadSceneAsync(sceneName);
+        async.allowSceneActivation = false;
+        if(Time.timeScale == 0)
+        {
+            Time.timeScale = 1;
+        }
+
+        while(!async.isDone)
+        {
+            if(async.progress>=0.9f)
+            {
+                if(!videoPlayer.isPlaying)
+                {
+                    async.allowSceneActivation = true;
+                }
+            }
+            yield return null;
+        }
     }
 }

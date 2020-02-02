d1477017cf0a84bd88ac65ffcff8c4256ec604e9
Tristan Smeets <tristansmeets89@hotmail.com>
2020-02-02 08:58:27 +0100

    Added a main menu pop up. Also added a main menu spawn script still needs to be added to pop up creator gameobject.

---
+++ GameJam2020/Assets/Scripts/UI/MainMenuSpawn.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuSpawn : MonoBehaviour
+{
+    PopupCreator popupCreator = null;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        popupCreator = GetComponent<PopupCreator>();
+        popupCreator.CreatePopup("MainMenu");
+    }
+}

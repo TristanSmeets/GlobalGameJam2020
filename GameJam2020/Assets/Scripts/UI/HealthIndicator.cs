fb816644b673ad3d5565ace4ff538719fec8c7cd
rpijpker01 <rimme.pijpker@gmail.com>
2020-02-02 09:58:03 +0100

    some fucking shit m8

--- GameJam2020/Assets/Scripts/UI/HealthIndicator.cs
+++ GameJam2020/Assets/Scripts/UI/HealthIndicator.cs
@@ -7,6 +7,7 @@ using Doozy.Engine.Progress;
 public class HealthIndicator : MonoBehaviour
 {
     private Progressor progressor;
+    private HealthComponent healthComponent;
     private float maxHealth = 100.0f;
     private Player.Player player;
     private const string playerTag = "Player";
@@ -17,7 +18,9 @@ public class HealthIndicator : MonoBehaviour
         progressor = GetComponentInChildren<Progressor>();
         progressor.AnimateValue = true;
         progressor.SetMax(player.GetPlayerStats().MaxHealth);
+        healthComponent = player.GetComponent<HealthComponent>();
         AddListeners();
+
     }
 
     private void OnDisable()
@@ -28,10 +31,17 @@ public class HealthIndicator : MonoBehaviour
     private void AddListeners()
     {
         player.DamagedPlayer += OnDamagedPlayer;
+        GameStats.OnRoundStart += UpdateMaxHealth;
     }
     private void RemoveListeners()
     {
         player.DamagedPlayer -= OnDamagedPlayer;
+        GameStats.OnRoundStart -= UpdateMaxHealth;
+    }
+
+    private void UpdateMaxHealth()
+    {
+        progressor.SetMax(healthComponent.GetMaxHealth());
     }
 
     private void OnDamagedPlayer(float currentHealth)
